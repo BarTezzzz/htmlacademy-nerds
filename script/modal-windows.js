ee083dc87be89fda952f66972a31d6779bfae9ad
Marchenko Maxim <slayer.jk@mail.ru>
2017-01-20 15:48:12 +0600

    Added jQuery - code fixed- add overlay for modal window- now truly modal.

--- script/modal-windows.js
+++ script/modal-windows.js
@@ -1,50 +1,18 @@
-/*jslint node: true */
-'use strict';
-
-var link = document.querySelector(".js-form");
-var popup = document.querySelector(".modal-content_form");
-var close = popup.querySelector(".modal-content_close");
-var form = popup.querySelector("form");
-var name = popup.querySelector("[name=name]");
-var email = popup.querySelector("[name=email]");
-var message = popup.querySelector("[name=message]");
-var storage = localStorage.getItem("email");
-var storage = localStorage.getItem("name");
-
-link.addEventListener("click", function (event) {
-  event.preventDefault();
-  popup.classList.add("modal-content_show");
-  if (storage) {
-    name.value = storage;
-    email.value = storage;
-    email.focus();
-  } else {
-    name.focus();
-  }
-});
-
-close.addEventListener("click", function (event) {
-  event.preventDefault();
-  popup.classList.remove("modal-content_show");
-  popup.classList.remove("modal-content_error");
-});
-
-form.addEventListener("submit", function (event) {
-  if (!name.value || !email.value) {
+$(document).ready(function () {
+  'use strict';
+   
+  $('.js-form').click(function () {
     event.preventDefault();
-    popup.classList.remove("modal-content_error");
-    popup.classList.add("modal-content_error");
-  } else {
-    localStorage.setItem("name", name.value);
-    localStorage.setItem("email", email.value);
-  }
-});
-
-window.addEventListener("keydown", function (event) {
-  if (event.keyCode === 27) {
-    if (popup.classList.contains("modal-content_show")) {
-      popup.classList.remove("modal-content_show");
-      popup.classList.remove("modal-content_error");
-    }
-  }
+    
+    $('.overlay').show();
+    $('.modal-content_form').addClass('modal-content_show');
+  });
+  
+  $('.modal-content_close').click(function () {
+    event.preventDefault();
+    
+    $('.overlay').hide();
+    $('.modal-content_form').removeClass('modal-content_show');
+  });
+  
 });
